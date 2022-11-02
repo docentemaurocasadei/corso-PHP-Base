4d2085d4a63e4df8ecf137653e749a7e9893b7e2
mauro <mauro.casadei@soluzionesoftware.com>
2022-11-02 12:42:33 +0100

    ex1

---
+++ lezione9/ex1.php
@@ -0,0 +1,14 @@
+<?php
+// creare una serie di variabili:
+/**
+ * $i di tipo intero valore 20
+ * $f di tipo float valore 50.9
+ * $s di tipo stringa valore 'ciao'
+ * $ar di tipo array valore 10,20,30
+ * $n di tipo null
+ * $b di tipo booleano valore false 
+ * 
+ * stampare ogni variabile con il suo nome
+ * es: i=20
+ * e a capo la variabile successiva
+ */
---
+++ lezione9/lezione9.php
@@ -0,0 +1,48 @@
+<?php
+//stampo ciao
+// print 'ciao\n';
+// #stampo ciao e newline
+// print "ciao\n";
+// /* stampo prova
+//  * posso fare più righe di commento
+//  */
+// print "prova";
+// //variabili
+// //iniziano con $
+// $mia=5;
+// $_i=10;
+// // $mia-var=8; NON CORRETTO il -
+// //$12a=15; NON CORRETTO il numero come 1 carattere
+// $a12=15;
+// $a=5;   //PHP è case sensitive
+// $A=10;
+// $ciao=null;
+
+// $int=5; //intero
+// $f=5.5; //float
+// $s="ciao"; //string
+// $b=true; //boolean
+// $ar=[1,2,3];    //array
+// $n=null;    //null type
+// print "\n";
+// print PHP_INT_MIN . "|" . PHP_INT_MAX;
+// print "\n";
+// echo PHP_INT_MIN, "|", PHP_INT_MAX;
+
+// $v = "mario";
+// print "{$v} ha 20 anni";
+// print "\n";
+
+// print "1 {$v} ha un maglione \"giallo\"";
+// print "\n";
+// print '2 {$v} ha un maglione \"giallo\"';
+
+$testo="oggi è mercoledì 
+domani sarà giovedì";
+print $testo;
+print "\n";
+//creo un array
+$ar=['marco','simone','silvia'];
+print_r($ar);
+print $ar[2];
+$ar2 = array(1,2,3);
