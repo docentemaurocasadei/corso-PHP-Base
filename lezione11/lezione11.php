b2cdd7d85966541154a2bf7738de7fb130187574
mauro <mauro.casadei@soluzionesoftware.com>
2022-11-07 11:28:43 +0100

    aggiunta insert su tab timbrature

--- lezione11/lezione11.php
+++ lezione11/lezione11.php
@@ -4,32 +4,39 @@ function stampa($valore){
 }
 //gestire le stringhe
 //strlen()
-$var="siamo a Fano";
-stampa(strlen($var));
-stampa(substr($var,2,3));
+// $var="siamo a Fano";
+// stampa(strlen($var));
+// stampa(substr($var,2,3));
 
-// $var = substr_replace($var, 'Pesaro', 8);
+// // $var = substr_replace($var, 'Pesaro', 8);
+// // stampa($var);
+
+// $var = str_replace('Fano', 'Pesaro', $var);
 // stampa($var);
 
-$var = str_replace('Fano', 'Pesaro', $var);
-stampa($var);
+// $input="l'aquilone è verde";
+// $input=addslashes($input); //l\'aquilone è verde
+// stampa($input);
+// //funzioni matematiche
+// $numeri=[1,5,9,7,4,2];
+// stampa(max($numeri));
+// $max=0;
+// foreach ($numeri as $numero){
+//     if ($numero>$max){
+//         $max=$numero;
+//     }
+// }
+// stampa($max);
+// $numero=3.184896;
+// //3.184 => 3.18
+// //3.185 => 3.19
+// stampa(round($numero,2));
+// //tenta la fortuna
 
-$input="l'aquilone è verde";
-$input=addslashes($input); //l\'aquilone è verde
-stampa($input);
-//funzioni matematiche
-$numeri=[1,5,9,7,4,2];
-stampa(max($numeri));
-$max=0;
-foreach ($numeri as $numero){
-    if ($numero>$max){
-        $max=$numero;
-    }
+$ar=[1,2,3];
+if (is_array($ar)){
+    stampa('ar è un array');
+}else{
+    stampa('ar NON è un array');
 }
-stampa($max);
-$numero=3.184896;
-//3.184 => 3.18
-//3.185 => 3.19
-stampa(round($numero,2));
-//tenta la fortuna
 
