d9da06abcb96fc76b0f9ac03895456eb20c040af
mauro <mauro.casadei@soluzionesoftware.com>
2022-11-07 10:51:40 +0100

    lezione 11 ex1 completo

--- lezione11/ex1.php
+++ lezione11/ex1.php
@@ -6,3 +6,11 @@
  * estrarre tramite rand(1,10) un numero da 1 a 10
  * stampare a video se l'utente ha vinto oppure no
  */
+
+$numero = $_GET['numero'];
+$estratto = rand(1,10);
+if ($numero == $estratto){
+    print "Hai Vinto!";
+}else{
+    print "Hai Perso, il vincente era $estratto!";
+}
